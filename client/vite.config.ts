442e5263450c8ddbecd4dc2cc8cfcf8adf447cb3
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2025-09-08 17:48:35 +0300

    Init TailwindCss & Shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        port: 5000,
+        strictPort: true, // optional: fail if port is already in use
+    },
 })
